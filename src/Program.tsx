e29186b96e3ec278fa06834f4e1ae2bccb4debc0
adammertel <12932677+adammertel@users.noreply.github.com>
2023-07-12 21:36:38 -0400

    Add program content

--- src/Program.tsx
+++ src/Program.tsx
@@ -1,10 +1,14 @@
-import { Container, Heading } from "react-bulma-components";
+import { Block, Container, Heading } from "react-bulma-components";
 
 function Program() {
   return (
     <div style={{}} className="page-section" id="section-program">
       <Container>
         <Heading size={3}>Program</Heading>
+        <Block></Block>
+        <Block>
+          <img src="program.png" />
+        </Block>
       </Container>
     </div>
   );
