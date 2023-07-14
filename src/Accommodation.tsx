7b7c7b843cd300e36e632423f6fc75b36a228cf3
adammertel <12932677+adammertel@users.noreply.github.com>
2023-07-14 10:50:42 -0400

    Fix accommodation formulation

--- src/Accommodation.tsx
+++ src/Accommodation.tsx
@@ -6,12 +6,12 @@ function Accomodation() {
       <Container>
         <Heading size={3}>Ubytovanie</Heading>
         <Block>
-          V areáli svadby bude možné ubytovať iba malý počet hostí; pre
-          niektorých ďalších máme na noc zo soboty na nedeľu bývanie v okolitých
-          obciach. Ubytovať sa bude možné v sobotu od 15, odubytovanie potom
-          bude v nedeľu do 15. Aby sme vedeli zorganizovať dostupné kapacity,
-          uveďte prosím vo formulári, či budete chcieť využiť nami rezervované
-          ubytovanie alebo máte ubytovanie vyriešené sami.
+          V areáli svadby bude možné ubytovať iba malý počet hostí; pre ďalších
+          máme na noc zo soboty na nedeľu bývanie v okolitých obciach. Ubytovať
+          sa bude možné v sobotu od 15, odubytovanie potom bude v nedeľu do 15.
+          Aby sme vedeli zorganizovať dostupné kapacity, uveďte prosím vo
+          formulári, či budete chcieť využiť nami rezervované ubytovanie alebo
+          máte ubytovanie vyriešené sami.
         </Block>
         <Block>
           Transport medzi ubytovaním, Kostkou v Poli, prípadne ďalšími okolitými
