0c716f868f938b12c0afcbd3ba456f22d308e81e
adammertel <12932677+adammertel@users.noreply.github.com>
2023-08-06 14:15:08 +0200

    Add photo copyright

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,19 +1,47 @@
 import { Footer, Heading } from "react-bulma-components";
+import { FaInstagram, FaLink } from "react-icons/fa";
 
 function FooterPart() {
   return (
     <>
       <div id="Footer" style={{ textAlign: "center" }}>
         <Footer>
-          <img
-            src="footer-resized.jpg"
-            style={{ width: "fit-content", height: "auto" }}
-          />
           <Heading subtitle size={1}>
             Tešíme sa na vás!
           </Heading>
           <Heading subtitle size={3}>
             Petra & Adam
+          </Heading>
+          <img
+            src="footer-resized.jpg"
+            style={{ width: "fit-content", height: "auto" }}
+          />
+          <Heading subtitle size={4} className="photo-copyright">
+            <i>
+              Fotky boli zhotovené šikovnou pani fotografkou{" "}
+              <b>Chaňovic fotí</b>
+            </i>
+            <div
+              style={{
+                marginLeft: "4px",
+                display: "inline",
+                padding: "10px",
+                verticalAlign: "top",
+                fontWeight: "bold",
+              }}
+            >
+              <a
+                href="https://www.instagram.com/chanovicfoti/?hl=en"
+                target="_blank"
+              >
+                <FaInstagram />
+                instagram
+              </a>
+              <a href="https://chanovicfoti.cz/" target="_blank">
+                <FaLink />
+                webstranka
+              </a>
+            </div>
           </Heading>
         </Footer>
       </div>
